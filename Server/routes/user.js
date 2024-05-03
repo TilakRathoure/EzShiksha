363e81d3567e232b4d8492a16203ae76b6cd8313
Tilak Rathoure <tilakrathoure@gmail.com>
2024-05-03 10:39:30 +0530

    new commit

---
+++ Server/routes/user.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { getMyProfile, login, logout, nice, register } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import Notemaking from "../Fornotemaking.js";
+import Grammerly from "../Forgrammerly.js";
+import Extract from "../ForExtracting.js";
+import multer from "multer";
+import Solve from "../ForSolving.js";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'C:/Work'); // Destination directory
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // File name
+    }
+  });
+  
+  const upload = multer({ storage: storage });
+
+router.post("/upload",upload.single('image'),Extract)
+
+router.post("/solve",upload.single('imagesolve'),Solve)
+
+
+router.get("/nice",nice);
+router.post("/note",Notemaking)
+router.post("/gram",Grammerly)
+
+router.post("/new", register);
+router.post("/login", login);
+
+router.get("/logout",isAuthenticated, logout);
+
+router.get("/me", isAuthenticated, getMyProfile);
+
+export default router;
