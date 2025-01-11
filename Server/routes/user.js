d313623a21fe13cf5509e6feae8157c08d586f67
Tilak Rathoure <tilakrathoure@gmail.com>
2025-01-11 10:17:37 +0530

    add uploads

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads');
+    cb(null, '/tmp/uploads');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
